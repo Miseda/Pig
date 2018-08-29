98d9a6c4e705564a083848a173e77b3ea78abee5
rsakhi <rsakhi>
2018-08-29 14:37:50 +0530

    set initial score of both players

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-	
+var score, activePlayer, roundScore;
+score = [0, 0];
+activePlayer = 0;
+roundScore = 0;
+
+document.querySelector("#score-0").textContent = "0";
+document.querySelector("#score-1").textContent = "0";
+document.querySelector("#current-0").textContent = "0";
+document.querySelector("#current-1").textContent = "0";
