2e6cc72d7af2375f2d15619c85ca7375f3bc5df6
rsakhi <rsakhi>
2018-08-29 15:18:26 +0530

    implimented hold btn functionality

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ document.querySelector("#current-1").textContent = "0";
 
 document.querySelector("#dice-1").style.display = "none";
 
+// roll dice for active player
 document.querySelector(".btn-roll").addEventListener('click', function(){
 	console.log("rolled")
 	var dice = Math.floor(Math.random()*6) + 1 //Genrate random number b/w 1 to 6
@@ -26,5 +27,19 @@ document.querySelector(".btn-roll").addEventListener('click', function(){
 		activePlayer = activePlayer == 1 ? 0 : 1;
 		console.log(activePlayer);
 	}
-	
 })
+
+//Hold the round value
+document.querySelector(".btn-hold").addEventListener('click', function(){
+	score[activePlayer] += roundScore;
+	document.querySelector("#score-"+activePlayer).textContent = score[activePlayer];
+	roundScore = 0;
+	var diceDoc = document.querySelector("#dice-1");
+	diceDoc.style.display = "none";
+	document.querySelector("#current-"+activePlayer).textContent = 0;
+	activePlayer = activePlayer == 1 ? 0 : 1;
+})
+
+
+
+
