0b61d1e44129259555167a72477e51d28ce50da3
rsakhi <sakhi.rohit@gmail.com>
2018-09-02 18:47:18 +0530

    Replace querySelector with ElementSelector

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ document.querySelector('.btn-new').addEventListener('click',init);
 document.querySelector(".btn-roll").addEventListener('click', function(){
 	console.log("rolled")
 	var dice = Math.floor(Math.random()*6) + 1 //Genrate random number b/w 1 to 6
-	var diceDoc = document.querySelector("#dice-1");
+	var diceDoc = document.getElementById("dice-1");
 	diceDoc.style.display = "block";
 	diceDoc.src = 'dice-' + dice + '.png';
 	if(dice != 1){
 		roundScore += dice;
-		document.querySelector("#current-"+activePlayer).textContent = roundScore;
+		document.getElementById("current-"+activePlayer).textContent = roundScore;
 	}else{
 		nextPlayer();
 	}
@@ -21,12 +21,12 @@ document.querySelector(".btn-roll").addEventListener('click', function(){
 //Hold the round value
 document.querySelector(".btn-hold").addEventListener('click', function(){
 	score[activePlayer] += roundScore;
-	document.querySelector("#score-"+activePlayer).textContent = score[activePlayer];
+	document.getElementById("score-"+activePlayer).textContent = score[activePlayer];
 	if(score[activePlayer] > 100){
-		document.querySelector("#name-"+activePlayer).textContent = "Winner!";
+		document.getElementById("name-"+activePlayer).textContent = "Winner!";
 		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
 		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-		document.querySelector("#dice-1").style.display = "none";
+		document.getElementById("dice-1").style.display = "none";
 		document.querySelector(".btn-hold").disabled = ture
 		document.querySelector(".btn-roll").disabled = ture
 	}else{
@@ -37,8 +37,8 @@ document.querySelector(".btn-hold").addEventListener('click', function(){
 
 function nextPlayer(){
 		roundScore = 0;
-		document.querySelector("#dice-1").style.display = "none";
-		document.querySelector("#current-"+activePlayer).textContent = 0;
+		document.getElementById("dice-1").style.display = "none";
+		document.getElementById("current-"+activePlayer).textContent = 0;
 		activePlayer = activePlayer == 1 ? 0 : 1;
 		document.querySelector(".player-0-panel").classList.remove('active');
 		document.querySelector(".player-1-panel").classList.remove('active');
@@ -47,7 +47,7 @@ function nextPlayer(){
 
 
 function init() {
-    scores = [0, 0];
+    score = [0, 0];
     activePlayer = 0;
     roundScore = 0;
 	
